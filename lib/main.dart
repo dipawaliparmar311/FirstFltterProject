933b631646da819b6e98e898ebda066cf15da4a2
Dipawali-2016 <dipali.parmar740@gmail.com>
2024-02-14 14:55:09 +0530

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myfirst_flutter_project/home_page.dart';
 
 void main()
 {
@@ -9,13 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-          child: Text("Hare Krishna Hare Krishna Krishna kRishna hare hare "),
-              ),
-        ),
-      ));
+      home: HomePage(),
+      );
   }
 }
