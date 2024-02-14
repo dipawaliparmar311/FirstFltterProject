a9dcea96864ff6d053315b5ec84969caf7712f67
Dipawali-2016 <dipali.parmar740@gmail.com>
2024-02-14 14:26:21 +0530

    my firt commit

---
+++ lib/practise.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main()
+{
+  print("Welcome to my first practise session ");
+  stdout.write("Please Enter you name");
+  var name=stdin.readLineSync();
+
+  print("name :$name");
+}
