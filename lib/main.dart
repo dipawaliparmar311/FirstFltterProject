727d172b784babd52dd576bce06c9af8a3d02cb0
Dipawali-2016 <dipali.parmar740@gmail.com>
2024-02-15 10:05:55 +0530

    Animated login button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:myfirst_flutter_project/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myfirst_flutter_project/pages/home_page.dart';
+import 'package:myfirst_flutter_project/pages/login_page.dart';
+import 'package:myfirst_flutter_project/utils/MyRoutes.dart';
 
 void main()
 {
@@ -12,7 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: HomePage(),
+      themeMode:ThemeMode.light,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue,fontFamily: GoogleFonts.lato().fontFamily),
+      routes: {
+        "/":(context) =>LoginPage(),
+        MyRoutes.loginRoute:(context)=> LoginPage(),
+        MyRoutes.homeRoute:(context)=> HomePage()
+      },
+
       );
   }
 }
